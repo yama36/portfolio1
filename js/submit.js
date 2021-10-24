e8f7c30ac5493a1c77fdff5ab6512b1b50cd094f
yama <87622650+yama36@users.noreply.github.com>
2021-10-24 18:40:07 +0900

    Add files via upload

---
+++ js/submit.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/hogehoge",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".end-message").slideDown();
+          $(".submit-btn").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
