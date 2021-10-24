6c4624ec774d6e905f930ddf885e99464ed625ce
yama <87622650+yama36@users.noreply.github.com>
2021-10-24 19:09:40 +0900

    Add files via upload

--- js/submit.js
+++ js/submit.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $('#form').submit(function (event) {
     var formData = $('#form').serialize();
     $.ajax({
-      url: "https://docs.google.com/forms/hogehoge",
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScXPvWvOJbZuMEa_L7ENd6Obd17J2R5lnMZkdhGppLZEnf9fw/formResponse",
       data: formData,
       type: "POST",
       dataType: "xml",
